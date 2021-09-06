dc2a0c29a85fb025e0ece6cd46c1f43095967109
inte168 <intelligent1001@chungbuk.ac.kr>
2021-09-06 21:14:33 +0900

    rename 카테코리 이름(#6)

--- src/data.js
+++ src/data.js
@@ -1,15 +1,16 @@
-import {fundamentalsCollection} from "./data/lecture.js";
-import {webcomponentsCollection} from "./data/web-components.js";
-import {accessibilityCollection} from "./data/accessibility.js";
-import {pwaCollection} from "./data/pwa.js";
-import {architectureCollection} from "./data/architecture-and-paradigmes.js";
-import {buildtoolsCollection} from "./data/build-tools.js";
-import {testingCollection} from "./data/testing.js";
-import {frameworksAndLibrariesCollection} from "./data/frameworks-and-libraries.js";
-import {modernCollection} from "./data/modern.js";
-import {teamCollaborationCollection} from "./data/team-collaboration.js";
-import {algorithmsCollection} from "./data/algorithms.js";
-import {designAndUXCollection} from "./data/design-and-ux.js";
+
+import {fundamentalsCollection} from "./data/lecture.js";
+import {major} from "./data/major.js";
+import {cultural} from "./data/cultural.js";
+import {pwaCollection} from "./data/pwa.js";
+import {architectureCollection} from "./data/architecture-and-paradigmes.js";
+import {buildtoolsCollection} from "./data/build-tools.js";
+import {testingCollection} from "./data/testing.js";
+import {frameworksAndLibrariesCollection} from "./data/frameworks-and-libraries.js";
+import {modernCollection} from "./data/modern.js";
+import {teamCollaborationCollection} from "./data/team-collaboration.js";
+import {algorithmsCollection} from "./data/algorithms.js";
+import {designAndUXCollection} from "./data/design-and-ux.js";
 import {databasesCollection} from "./data/databases.js";
 
 /**
@@ -17,8 +18,8 @@ import {databasesCollection} from "./data/databases.js";
  */
 export const collections = [
 	fundamentalsCollection,
-	accessibilityCollection,
-	webcomponentsCollection,
+	cultural,
+	major,
 	pwaCollection,
 	buildtoolsCollection,
 	frameworksAndLibrariesCollection,
